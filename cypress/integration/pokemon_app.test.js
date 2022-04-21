b15509db64bcbd8f01f4e3cc1f71891e7dbdcb1f
Akira Takeshi <akiratakeshi@gmx.com>
2022-04-21 15:19:47 +0200

    Heroku app

---
+++ cypress/integration/pokemon_app.test.js
@@ -0,0 +1,7 @@
+describe('Cypress - Pokemon app', () => {
+  it('should open the application', () => {
+    cy.visit('http://localhost:8080');
+    cy.contains('bulbasaur');
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.');
+  });
+});
