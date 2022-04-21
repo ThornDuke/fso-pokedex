401b462464c3795f474c6a2c4eb7c8d656702a05
Akira Takeshi <akiratakeshi@gmx.com>
2022-04-21 15:59:47 +0200

    Job 12

--- cypress/integration/pokemon_app.test.js
+++ cypress/integration/pokemon_app.test.js
@@ -1,7 +1,9 @@
+import { cypress as cy } from 'cypress'
+
 describe('Cypress - Pokemon app', () => {
   it('should open the application', () => {
-    cy.visit('http://localhost:8080');
-    cy.contains('bulbasaur');
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.');
-  });
-});
+    cy.visit('http://localhost:8080')
+    cy.contains('bulbasaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
